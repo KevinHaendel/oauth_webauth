8edb62874df455f16c15042a6bb2dab53cb5505f
Kevin Händel <kevinhaendel@gmail.com>
2024-12-01 22:11:39 +0100

    Fix failed import

--- lib/src/utils/cross_platform_support.dart
+++ lib/src/utils/cross_platform_support.dart
@@ -1,3 +1,1 @@
-export 'cross_platform_support_noop.dart'
-    if (dart.library.html) 'cross_platform_support_html.dart'
-    if (dart.library.web) 'cross_platform_support_web.dart';
+export 'cross_platform_support_web.dart';
